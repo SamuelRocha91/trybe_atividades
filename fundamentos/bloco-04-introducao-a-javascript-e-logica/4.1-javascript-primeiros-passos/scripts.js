0fb8b660e1541938ceb840bec68dad96b921cfb8
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-21 14:53:28 -0300

    exercicio sobre condicionais

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/scripts.js
@@ -30,3 +30,35 @@ console.log(area)
 
 const perimeter = 2*base + 2*heigth;
 console.log(perimeter)
+
+// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100
+
+const nota = 60;
+
+// Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se essa pessoa está na lista de espera. Para isso, considere as seguintes informações:
+  //Se a nota for maior ou igual a 80, imprima “Parabéns, você foi aprovada(o)!”
+  //Se a nota for menor que 80 e maior ou igual a 60, imprima “Você está na nossa lista de espera”
+  //Se a nota for menor que 60, imprima “Você foi reprovada(o)”
+  if(nota >=80){
+    console.log("Parabéns, você foi aprovado(a)!")
+  }
+  else if(nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera")
+  }
+  else{
+    console.log("Você foi reprovado(a)!")
+  }
+  // Altere o valor da nota para verificar se as condições que você implementou funcionam.
+  
+  nota = 70;
+
+  if(nota >=80){
+    console.log("Parabéns, você foi aprovado(a)!")
+  }
+  else if(nota < 80 && nota >= 60){
+    console.log("Você está na nossa lista de espera")
+  }
+  else{
+    console.log("Você foi reprovado(a)!")
+  }
+  // Dá erro pelo identificador 'nota' ser uma constante.
