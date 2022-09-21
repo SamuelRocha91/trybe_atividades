68ca383b5b68b135db01814504f03c1605d9f846
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-21 13:35:51 -0300

    criando variavel para ano de nascimento

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/script.js
@@ -5,3 +5,7 @@ const myName = "Samuel";
 // Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
 
 const birthCity = "Salvador";
+
+// Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+
+let birthYear = 1991;
