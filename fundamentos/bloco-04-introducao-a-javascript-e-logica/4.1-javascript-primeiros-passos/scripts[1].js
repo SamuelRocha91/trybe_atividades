1d7f9f2f4d5495f84c4bad43e16ea338ab8f61a6
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-21 15:40:13 -0300

    exercício 02 - maior de dois números

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/scripts[1].js
@@ -0,0 +1,11 @@
+   // Utilize if/else para fazer um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+   
+   const a = 12;
+   const b = 15
+
+   if(a > b){
+    console.log(`${a} é maior que ${b}`)
+   }
+   else{
+    console.log(`${b} é maior que ${a}`)
+   }
