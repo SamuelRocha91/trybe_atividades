24b4add94e4dfbd96eb9c961e6e48c3c393780f5
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-21 14:38:03 -0300

    cria exercícios de operadores aritméticos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.1-javascript-primeiros-passos/scripts.js
@@ -12,3 +12,21 @@ console.log(typeof patientId)
 console.log(typeof isEnrolled)
 console.log(typeof patientInfo)
 console.log(typeof patientEmail)
+
+// Crie uma constante chamada base e atribua a ela o valor 5.
+
+const base = 5;
+
+// Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+
+const heigth = 8;
+
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+
+const area = base * heigth;
+console.log(area)
+
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const perimeter = 2*base + 2*heigth;
+console.log(perimeter)
