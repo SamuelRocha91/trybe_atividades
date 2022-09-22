a132325688bf145fe4bf42ea7c114758006a217c
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-22 14:52:58 -0300

    exercício 06 - encontrando qtdade de ímpares num array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.2-array-e-for-loop/script[0].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.2-array-e-for-loop/script[0].js
@@ -36,3 +36,18 @@ for(let index = 0; index < numbers.length; index = index + 1){
   }
 }
 console.log(maior)
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado
+
+let somaDeImpares = 0
+for(let index = 0; index < numbers.length; index = index + 1){
+  if(numbers[index] % 2 === 1){
+    somaDeImpares = somaDeImpares + 1;
+  }
+}
+if(somaDeImpares === 0){
+  console.log("nenhum valor ímpar encontrado")
+}
+else{
+  console.log(`Existem ${somaDeImpares} valores ímpares no array numbers`)
+}
