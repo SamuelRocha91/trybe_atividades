638888a65500b64134328e85fdad13b539f45832
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-22 14:29:49 -0300

    exercício 01 - percorrendo o array

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.2-array-e-for-loop/script[0].js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log()
+
+for(let index = 0; index < numbers.length; index = index + 1){
+  console.log(numbers[index])
+}
+
+// Some todos os valores contidos no array e imprima o resultado;
