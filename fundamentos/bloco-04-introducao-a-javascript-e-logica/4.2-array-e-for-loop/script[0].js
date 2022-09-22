74000f51a2397e39ef0c4395d31617adb962ad00
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-22 14:41:56 -0300

    exercício 04 - uso de if/else

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.2-array-e-for-loop/script[0].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.2-array-e-for-loop/script[0].js
@@ -7,6 +7,7 @@ for(let index = 0; index < numbers.length; index = index + 1){
 }
 
 // Some todos os valores contidos no array e imprima o resultado;
+
 let soma = 0;
 for(let index = 0; index < numbers.length; index = index + 1){
   soma = soma + numbers[index]
@@ -17,3 +18,12 @@ console.log(`A soma dos elementos do array numbers é igual a ${soma}`)
 
 media = soma / numbers.length
 console.log(`A média aritmética do array numbers é igual a ${media}`)
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+if(media > 20){
+  console.log("valor maior que 20");
+}
+else{
+  console.log("valor menor ou igual a 20")
+}
