b6818d1ed6a91f57b6bb7905239d793789d15ece
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-23 13:13:24 -0300

    retificando exercício 01

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
@@ -1,6 +1,6 @@
 /* Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10 */
 let fatorial = 10;
-let resultado = 1;
+let resultado = 10;
 for(let index = fatorial - 1; index > 0; index = index - 1){
   if( index !== 1){
   fatorial = fatorial + ' * ' + index;
