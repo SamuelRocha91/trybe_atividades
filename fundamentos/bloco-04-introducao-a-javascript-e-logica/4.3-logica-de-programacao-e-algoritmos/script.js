6116286773309cf003954bab0375b106f729a8f3
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-23 13:19:23 -0300

    exercício 02 - inversão de palavras

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
@@ -11,3 +11,12 @@ for(let index = fatorial - 1; index > 0; index = index - 1){
     resultado = resultado * index;
 }
 console.log(`10!=${fatorial}${resultado}`)
+
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let invertida = "";
+for(let index = word.length - 1; index >= 0; index = index - 1){
+  invertida = invertida + word[index];
+}
+console.log(invertida)
