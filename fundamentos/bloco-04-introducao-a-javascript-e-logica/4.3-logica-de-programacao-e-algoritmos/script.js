a0cdd6afac1a83d9ccf71f22c4953d5224725a74
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-23 13:29:42 -0300

    exercício 03 - parte 01 - maior palavra de um array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.3-logica-de-programacao-e-algoritmos/script.js
@@ -20,3 +20,16 @@ for(let index = word.length - 1; index >= 0; index = index - 1){
   invertida = invertida + word[index];
 }
 console.log(invertida)
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos: um que retorne a maior palavra desse array e outro que retorne a menor. Considere o número de caracteres de cada palavra
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorP = "";
+
+for(let index = 0; index < array.length; index= index + 1){
+  if(maiorP.length < array[index].length){
+    maiorP = array[index];
+  }
+}
+console.log(`A maior palavra do Array é ${maiorP}`)
