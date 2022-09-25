ebafdc4403cec82e1f5b3d5e9710647be7ae6042
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 18:05:01 -0300

    Bônus 01 - conversor de romanos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[04].js
@@ -0,0 +1,38 @@
+/* (Bônus) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa. */
+
+let word = "DCXIII" 
+let algarismosRomanos = {
+ I: 1,
+ IV: 4,
+ V: 5,
+ IX: 9,
+ X: 10,
+ XL: 40,
+ L: 50,
+ XC:90,
+ C: 100,
+ CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
+}
+function conversorDeRomanos(string){
+let sum = 0;
+let wikipedia = [];
+let termo = 0;
+for(let index = 0; index < string.length; index = index + 1){
+wikipedia.push(algarismosRomanos[string[index]])
+}  
+for(let index = string.length -1; index >= 0; index= index - 1){
+    if( wikipedia[index] >= termo){
+      sum = sum + wikipedia[index];
+    }
+    else{
+      sum = sum - wikipedia[index];
+    }
+    termo = wikipedia[index]
+  }
+  return sum
+}
+console.log(conversorDeRomanos("CDXI"))
+ 
