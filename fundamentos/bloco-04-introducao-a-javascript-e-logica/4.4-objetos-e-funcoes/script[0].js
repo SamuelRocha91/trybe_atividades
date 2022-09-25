e21d2c8521daa545000e1c99d672fb45942a16aa
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 08:06:35 -0300

    adcicionando nova informação a um array dentro de um objeto

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[0].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[0].js
@@ -58,3 +58,14 @@ let leitor = {
 };
 
 console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`)
+
+/* Adicione um novo livro favorito na chave livrosFavoritos, que é um array */
+
+leitor.livrosFavoritos.push(
+  {
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+}
+)
+console.log(leitor)
