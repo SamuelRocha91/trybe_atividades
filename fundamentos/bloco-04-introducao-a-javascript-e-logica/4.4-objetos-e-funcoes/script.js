acc6a148f74af1c0cbf0136b00e9b824ff800702
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 07:10:31 -0300

    retornando se o valor é positivo

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script.js
@@ -61,3 +61,19 @@ function maiorDeTres(n1, n2, n3){
 }
 
 console.log(maiorDeTres(a,b,c))
+
+/* Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”. */
+
+function positivoNegativo(n){
+  if(n > 0){
+    return `Positive`
+  }
+  else if (n < 0){
+    return `Negative`
+  }
+  else{
+    return `Zero`
+  }
+}
+
+console.log(positivoNegativo(a))
