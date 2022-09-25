6e5354b2614a76c3185ec5e7c86ecfbfc1800cdd
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 19:06:24 -0300

    retornando array de pares - bonus 02

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[04].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[04].js
@@ -1,6 +1,5 @@
 /* (Bônus) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa. */
 
-let word = "DCXIII" 
 let algarismosRomanos = {
  I: 1,
  IV: 4,
