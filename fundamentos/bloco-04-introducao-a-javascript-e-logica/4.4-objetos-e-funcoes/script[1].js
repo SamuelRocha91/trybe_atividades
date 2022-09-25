53f1f3f041787436581cdaa413ef347d02116cae
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 08:32:28 -0300

    Funções - índice do maior valor de um array

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[1].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[1].js
@@ -11,3 +11,20 @@ function verificaPalindromo(word){
   }
 }
 console.log(verificaPalindromo(palavra))
+
+/* Crie uma função que receba um array de inteiros e retorne o índice do maior valor */
+
+let teste = [2, 3, 6, 7, 10, 1];
+
+function indiceMaiorValor(array){
+  let maiorValor = array[0];
+  let indiceDoMaior = 0;
+  for(let index = 0; index < teste.length; index = index + 1){
+    if(array[index] > maiorValor ){
+      maiorValor = array[index];
+      indiceDoMaior = index;
+    }
+  }
+    return indiceDoMaior
+}
+console.log(indiceMaiorValor(teste))
