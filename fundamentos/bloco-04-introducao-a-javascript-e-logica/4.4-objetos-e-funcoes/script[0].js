e6d9dc6366985e8b08a9eba924c267e816869f16
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-09-25 07:42:05 -0300

    Objeto e for/in - 02  inserindo nova chave

--- fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[0].js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica/4.4-objetos-e-funcoes/script[0].js
@@ -7,3 +7,8 @@ let info = {
 Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. */
 
 console.log(`Seja bem-vida, ${info.personagem}`)
+
+/* Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. */
+
+info.recorrente = "sim";
+console.log(info)
