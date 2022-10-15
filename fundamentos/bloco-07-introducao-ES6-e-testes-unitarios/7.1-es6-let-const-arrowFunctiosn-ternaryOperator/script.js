34be5ea9e83c20a5b76766581109259a712209f4
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-15 19:21:08 -0300

    template literals

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.1-es6-let-const-arrowFunctiosn-ternaryOperator/script.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.1-es6-let-const-arrowFunctiosn-ternaryOperator/script.js
@@ -14,11 +14,11 @@ testingScope(true); */
 
 const testingScope = (escopo) => {
   if (escopo) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'
-    ifScope += ' ótimo, fui utilizada no escopo !'
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope}ótimo, fui utilizada no escopo !`
     console.log(ifScope);
   } else {
-    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    const elseScope = `Não devo ser utilizada fora do meu escopo (else)`;
     console.log(elseScope)
   }
 };
