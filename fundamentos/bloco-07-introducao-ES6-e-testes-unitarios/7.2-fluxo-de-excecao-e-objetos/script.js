1121338e0462330c645d9655cf1e6b2202b257ac
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-16 17:06:11 -0300

    funcao customerInfo

---
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/script.js
@@ -0,0 +1,52 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+/* “Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701” */
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const orderReal = Object.keys(order);
+  console.log(orderReal)
+  return console.log(`Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`);
+};
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+};
+
+orderModifier(order);
