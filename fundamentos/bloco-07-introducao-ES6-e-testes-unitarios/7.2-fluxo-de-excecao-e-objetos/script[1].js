0bbf7e943525b48ffb65586a1d67dda32a8fd37f
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-16 18:07:17 -0300

    object assign

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/script[1].js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.2-fluxo-de-excecao-e-objetos/script[1].js
@@ -24,21 +24,27 @@ function addTurn(objeto, chave, valor) {
 }
 
 addTurn(lesson2,'turno','noite')
-console.log(lesson2)
+//console.log(lesson2)
 
 /* Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro*/
 const listKeys = (objeto) => Object.keys(objeto);
-console.log(listKeys(lesson2))
+//console.log(listKeys(lesson2))
 
 /* Crie uma função para mostrar o tamanho de um objeto.
  */
 const objectLength = (objeto) => Object.entries(objeto).length
-console.log(objectLength(lesson2))
+//console.log(objectLength(lesson2))
 /* Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
  */
 const objectValues = (objeto) => Object.values(objeto)
-console.log(objectValues(lesson2))
+//console.log(objectValues(lesson2))
 
 /* Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3 */
-
-
+const modifier1 = {};
+const modifier2 = {};
+const modifier3 = {};
+modifier1.lesson1 = lesson1;
+modifier2.lesson2 = lesson2;
+modifier3.lesson3 = lesson3;
+const allLessons = Object.assign({}, modifier1, modifier2, modifier3)
+console.log(allLessons)
