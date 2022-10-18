5a549907d5ac1b1e2d657b7c64a28338a149f831
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-18 08:20:04 -0300

    exercício 04

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.js
@@ -16,4 +16,40 @@ function myRemove(arr, item) {
   return newArr;
 }
 
-module.exports = {sum, myRemove};
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+const vogais = ['a', 'e', 'i', 'o', 'u'];
+const code = [1, 2, 3, 4, 5];
+
+function encode(text) {
+  const caracters = text.split('');
+  caracters.forEach((letra, i) => {
+    vogais.forEach((vogal, k) => {
+      caracters[i] = (letra === vogal) ? code[k] : caracters[i];
+    });
+  });
+
+  return caracters.join('');
+}
+
+function decode(text) {
+  const caracters = text.split('');
+  caracters.forEach((caracter, i) => {
+    code.forEach((num, k) => {
+      caracters[i] = (caracter === num.toString()) ? vogais[k] : caracters[i];
+    });
+  });
+
+  return caracters.join('');
+}
+
+
+// implemente seus testes aqui
+
+module.exports = {sum, myRemove, myFizzBuzz, encode, decode};
--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const {sum, myRemove, myFizzBuzz, encode, decode} = require('./sum');
 
 describe('Requisito 01', () => {
   test('testa se 4 mais 5 é igual a 9', () => {
@@ -25,5 +25,44 @@ describe('Requisito 02', () => {
   })
   test('testa se ao passar o array [1, 2, 3, 4] e o item 5 se o retorno será de [1,2,3,4]', () => {
     expect(myRemove([1, 2, 3, 4], 5)).toEqual([1,2,3,4])
+  })
+})
+
+describe('Requisito 03', () => {
+  test('testa se ao passar o parâmetro 15 a função retorna fizzbuzz', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz')
+  })
+  test('testa se ao passar o parâmetro 6 a função retorna fizz', () => {
+    expect(myFizzBuzz(6)).toBe('fizz')
+  })
+  test('testa se ao passar o parâmetro 10 a função retorna buzz', () => {
+    expect(myFizzBuzz(10)).toBe('buzz')
+  })
+  test('testa se ao passar o parâmetro 11 a função retorna 11', () => {
+    expect(myFizzBuzz(11)).toBe(11)
+  })
+  test('testa se ao passar o parâmetro "a" a função retorna "false"', () => {
+    expect(myFizzBuzz('a')).toBe(false)
+  })
+})
+
+describe('Requisito 04', () => {
+  test('verifica se encode e decode são funções existentes', () => {
+    expect(typeof encode && typeof decode).toBe('function')
+  })
+  test('verifica se encode ao receber "aeiou" retorna "12345"', () => {
+    expect(encode('aeiou')).toBe('12345')
+  })
+  test('verifica se decode ao receber "12345" retorna "aeiou"', () => {
+    expect(decode('12345')).toBe('aeiou')
+  })
+  test('verifica se decode ao receber "6789" retorna "6789"', () => {
+    expect(decode('6789')).toBe('6789')
+  })
+  test('verifica se encode ao receber "snts" retorna "snts"', () => {
+    expect(encode('snts')).toBe('snts')
+  })
+  test('verifica se a string passada pela função possui o mesmo número de caracteres do retorno', () => {
+    expect(encode('samuel').length).toBe(6)
   })
 })
