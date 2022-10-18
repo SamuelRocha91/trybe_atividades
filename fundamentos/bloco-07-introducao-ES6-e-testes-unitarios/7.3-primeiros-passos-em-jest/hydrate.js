73cfa2fdd1775120d0085972c196e194882e0891
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-18 10:24:11 -0300

    função hydrate implementada

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/hydrate.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/hydrate.js
@@ -1,4 +1,12 @@
-const hydrate = () => {
-  
+const hydrate = (frase) => {
+  const array = frase.split(' ')
+  let total = 0;
+  array.forEach((element) => {
+    let inteiro = parseInt(element);
+    if (typeof inteiro === 'number' && !isNaN(inteiro)) {
+      total += inteiro;
+    }
+  });
+ return   total > 1 ?  `${total} copos de água` : '1 copo de água'
 };
 module.exports = hydrate;
