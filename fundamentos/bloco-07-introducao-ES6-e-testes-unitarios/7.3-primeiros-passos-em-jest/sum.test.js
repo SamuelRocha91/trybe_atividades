7c72da7ae92f4f8668fb573fbfe43255e304a2f7
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-17 21:38:33 -0300

    exercício 02

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+const {sum, myRemove} = require('./sum');
 
 describe('Requisito 01', () => {
   test('testa se 4 mais 5 é igual a 9', () => {
@@ -12,5 +12,18 @@ describe('Requisito 01', () => {
   })
   test(`testa se ao lançar 4 mais '5' a função retorna um erro específico`, () => {
     expect(() => sum(4, '5')).toThrow('parameters must be numbers');
+  })
+})
+
+
+describe('Requisito 02', () => {
+  test('testa se ao passar o array [1, 2, 3, 4] e o item 3 o retorno será de [1,2,4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1,2,4])
+  })
+  test('testa se ao passar o array [1, 2, 3, 4] e o item 3 o retorno  não será de [1,2,3,4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1,2,3,4])
+  })
+  test('testa se ao passar o array [1, 2, 3, 4] e o item 5 se o retorno será de [1,2,3,4]', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1,2,3,4])
   })
 })
