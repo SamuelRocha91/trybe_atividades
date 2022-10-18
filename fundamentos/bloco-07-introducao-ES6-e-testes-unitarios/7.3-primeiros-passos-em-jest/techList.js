4253d8cdefe434d9a7c1559d6b77b13c9405ed7f
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-18 09:04:36 -0300

    techList retorna um objeto ordenado

--- fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/techList.js
+++ fundamentos/bloco-07-introducao-ES6-e-testes-unitarios/7.3-primeiros-passos-em-jest/techList.js
@@ -1,8 +1,14 @@
 const techList = (array, texto) => {
+  const newArray = array.sort();
+  const arrayObjeto = [] ;
 
+  for(let index = 0; index < array.length; index += 1) {
+    arrayObjeto.push({name: texto, tech: newArray[index]}) 
+  }
+  return arrayObjeto
 }
 
-
+console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'))
 
 module.exports = techList;
 
