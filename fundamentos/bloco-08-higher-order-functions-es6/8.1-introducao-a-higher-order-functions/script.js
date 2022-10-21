33ff7583e510cf68c387277f91c2e1b067dc893f
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-21 17:02:15 -0300

    hof corretora

--- fundamentos/bloco-08-higher-order-functions-es6/8.1-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-08-higher-order-functions-es6/8.1-introducao-a-higher-order-functions/script.js
@@ -18,8 +18,8 @@ const newEmployees = (callBack) => {
   return employees;
 };
 
-console.log(newEmployees(createEmail))
-/* Sorteador de loteria
+/* console.log(newEmployees(createEmail))
+ *//* Sorteador de loteria
 Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns você ganhou”). */
 const validador = (num1, num2) => num1 === num2 ? 'Parabéns você ganhou' : 'Tente novamente'
 
@@ -27,4 +27,42 @@ const loteria = (callBack, apostado) => {
   const numberSorteado = Math.ceil(Math.random() * 5);
    return callBack(numberSorteado, apostado);
 }
-console.log(loteria(validador, 5))
+/* console.log(loteria(validador, 5))
+ */
+
+/* Crie uma HOF que receberá três parâmetros:
+
+O primeiro será um array de respostas corretas (soluções);
+
+O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+
+O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios:
+
+Uma resposta correta adiciona 1 ponto à pontuação final;
+
+A ausência de uma resposta não altera a pontuação (quando for “N.A”);
+
+Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+
+Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays: */
+
+function corretora(arraySol, arrayResp, callBack) {
+  return callBack(arraySol, arrayResp)
+}
+const terceirizada = (array1, array2) => {
+  let tot = 0
+  for (let index = 0; index < array1.length; index += 1) {
+    if (array1[index] === array2[index]) {
+      tot += 1;
+    } else if (array2[index] === 'N.A') {
+      tot += 0;
+    } else {
+      tot -= 0.5;
+    }
+  }
+  return tot
+}
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+console.log(corretora( RIGHT_ANSWERS, STUDENT_ANSWERS, terceirizada))
