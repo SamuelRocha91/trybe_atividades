5c7f3d066fd163bad36f9c69de744714392e75b3
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-11-09 15:01:18 -0300

    finalizando exercicio super heroes

--- front-end/bloco-01-javascript-e-testes-assincronos/1.2-javascript-assincrono-promises-e-fetch/src/index.js
+++ front-end/bloco-01-javascript-e-testes-assincronos/1.2-javascript-assincrono-promises-e-fetch/src/index.js
@@ -4,13 +4,13 @@ import './style.css'
 const img = document.querySelector('img');
 const p = document.querySelector('p');
 const button = document.querySelector('button');
-const BASE_URL = 'https://superheroapi.com/api/access-token/character-'
+const BASE_URL = 'https://superheroapi.com/api.php/101352002801358/'
 
-const generateNumber = () => Math.round(Math.random() * 600);
+const generateNumber = () => Math.round(Math.random() * 900);
 
 button.addEventListener('click', () => {
   const randomNumber = generateNumber();
-  fetch(`${BASE_URL}${randomNumber}`).then((response) => console.log(response.json()))
+  fetch(`${BASE_URL}${randomNumber}`).then((response) => response.json())
   .then((data) => {
     img.src = data.image.url;
     p.innerText = data.name;
