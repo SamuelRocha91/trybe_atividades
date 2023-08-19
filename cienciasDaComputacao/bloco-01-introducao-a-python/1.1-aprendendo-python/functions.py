1ee332ef9388a0eb59cd86bda6d7fe2655ddbaa4
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-08-19 13:37:28 -0300

    exercicio 03 - imprimindo quadrado

--- cienciasDaComputacao/bloco-01-introducao-a-python/1.1-aprendendo-python/functions.py
+++ cienciasDaComputacao/bloco-01-introducao-a-python/1.1-aprendendo-python/functions.py
@@ -10,3 +10,9 @@ def media(lista):
     return sum(lista) / len(lista)
 
 print(media([10, 5, 2, 3]))
+
+def imprimir_quadrado(n):
+    for i in range(1, n + 1):
+        print(n * '*')
+
+imprimir_quadrado(5)
